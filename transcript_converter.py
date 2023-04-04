4b1039f28d4e340ae7c9c1f5347c8a5fd37d0e1d
yashj <shine@159>
2023-04-04 09:46:59 +0530

    logic to convert Teams transcript to df

--- transcript_converter.py
+++ transcript_converter.py
@@ -1,3 +1,30 @@
-import pandas
+import docx2txt
+import pandas as pd
 
-print("Hello world")
+# Read the docx file using docx2txt library
+text = docx2txt.process(r"D:\ML\MoM_AI\sample_data\Communication_Training.docx")
+
+# print(text)
+# Split the text into lines
+lines = text.split('\n')
+
+# Create an empty dataframe with three columns
+df = pd.DataFrame(columns=["start_time", "end_time", "Speaker", "Text"])
+
+# Loop through the lines and extract the information
+for i in range(0, len(lines), 4):
+    # Extract the timestamp, speaker, and text information
+    timestamp = lines[i]
+    speaker = lines[i + 1]
+    text = lines[i + 2]
+
+    # Split the timestamp into start and end times
+    start_time, end_time = timestamp.split(" --> ")
+
+    # Add the information to the dataframe
+    df = df.append({"start_time": start_time.strip(), "end_time": end_time.strip(), "Speaker": speaker.strip(),
+                    "Text": text.strip()}, ignore_index=True)
+
+# Print the dataframe
+df.to_csv("D:\ML\MoM_AI\sample_data\converted_data\converted.csv")
+print(df)
